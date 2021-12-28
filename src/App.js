7851a3eee3537cbeddba6d33a05aac64081e0574
jeet1desai <jeetdesai995@gmail.com>
2021-12-28 11:13:25 +0530

    stripe-demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  PaymentRequestButtonElement,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const stripe = useStripe();
+  const [paymentRequest, setPaymentRequest] = useState(null);
+
+  useEffect(() => {
+    if (stripe) {
+      const pr = stripe.paymentRequest({
+        country: "US",
+        currency: "usd",
+        total: {
+          label: "Demo total",
+          amount: 1350,
+        },
+        requestPayerName: true,
+        requestPayerEmail: true,
+        requestShipping: true,
+        shippingOptions: [
+          {
+            id: "standard-global",
+            label: "Global shipping",
+            detail: "Arrives in 5 to 7 days",
+            amount: 350,
+          },
+        ],
+      });
+      // Check the availability of the Payment Request API first.
+      pr.canMakePayment().then((result) => {
+        if (result) {
+          // pr.on("paymentmethod", handlePaymentMethodReceived);
+          setPaymentRequest(pr);
+        }
+      });
+    }
+  }, [stripe]);
+
+  if (paymentRequest) {
+    return <PaymentRequestButtonElement options={{ paymentRequest }} />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +52,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Elements stripe={stripePromise}>
     <App />
-  </React.StrictMode>,
+  </Elements>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
