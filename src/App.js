f921982c5a131ade3d30e2d8c22dd3102da53112
jeet1desai <jeetdesai995@gmail.com>
2021-12-28 12:43:46 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {
-  PaymentRequestButtonElement,
-  useStripe,
-} from "@stripe/react-stripe-js";
 import React, { useEffect, useState } from "react";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from "@stripe/react-stripe-js";
 
 function App() {
   const stripe = useStripe();
-  const [paymentRequest, setPaymentRequest] = useState(null);
+  const elements = useElements();
 
-  useEffect(() => {
-    if (stripe) {
-      const pr = stripe.paymentRequest({
-        country: "US",
-        currency: "usd",
-        total: {
-          label: "Demo total",
-          amount: 1350,
-        },
-        requestPayerName: true,
-        requestPayerEmail: true,
-        requestShipping: true,
-        shippingOptions: [
-          {
-            id: "standard-global",
-            label: "Global shipping",
-            detail: "Arrives in 5 to 7 days",
-            amount: 350,
-          },
-        ],
-      });
-      // Check the availability of the Payment Request API first.
-      pr.canMakePayment().then((result) => {
-        if (result) {
-          // pr.on("paymentmethod", handlePaymentMethodReceived);
-          setPaymentRequest(pr);
-        }
-      });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
     }
-  }, [stripe]);
 
-  if (paymentRequest) {
-    return <PaymentRequestButtonElement options={{ paymentRequest }} />;
-  }
+    const result = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "https://my-site.com/order/123/complete",
+      },
+    });
 
+    if (result.error) {
+      // Show error to your customer (for example, payment details incomplete)
+      console.log(result.error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <PaymentElement />
+      <button disabled={!stripe}>Submit</button>
+    </form>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
+const options = {
+  // passing the client secret obtained from the server
+  clientSecret: process.env.REACT_APP_STRIPE_KEY,
+};
+
 ReactDOM.render(
-  <Elements stripe={stripePromise}>
+  <Elements stripe={stripePromise} options={options}>
     <App />
   </Elements>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
