d2a2f7a2e00a160d99ccf6026eb98e1cfb97caeb
jeet1desai <jeetdesai995@gmail.com>
2021-12-28 14:19:08 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { useEffect, useState } from "react";
-import {
-  useStripe,
-  useElements,
-  PaymentElement,
-} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "./CheckoutForm";
+import "./App.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(
+  "pk_test_51KBXjTSJiJf23hBTiqnwBG461YdFhGTbqxvqXzYlO2WQFK1o6H5ZH90d6SejXEbuFPPCzQYZQypSd1hv6A2jJArw00xOo2W8M2"
+);
 
 function App() {
-  const stripe = useStripe();
-  const elements = useElements();
+  const [clientSecret, setClientSecret] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
 
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
-
-    const result = await stripe.confirmPayment({
-      //`Elements` instance that was used to create the Payment Element
-      elements,
-      confirmParams: {
-        return_url: "https://my-site.com/order/123/complete",
-      },
-    });
-
-    if (result.error) {
-      // Show error to your customer (for example, payment details incomplete)
-      console.log(result.error.message);
-    } else {
-      // Your customer will be redirected to your `return_url`. For some payment
-      // methods like iDEAL, your customer will be redirected to an intermediate
-      // site first to authorize the payment, then redirected to the `return_url`.
-    }
+  const appearance = {
+    theme: "stripe",
   };
+  
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement />
-      <button disabled={!stripe}>Submit</button>
-    </form>
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+import App from './App';
 
-const options = {
-  // passing the client secret obtained from the server
-  clientSecret: process.env.REACT_APP_STRIPE_KEY,
-};
-
-ReactDOM.render(
-  <Elements stripe={stripePromise} options={options}>
-    <App />
-  </Elements>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
