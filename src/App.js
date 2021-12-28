29fdb1e466c1c635407ed9fb414cbefc753cfcfe
jeet1desai <jeetdesai995@gmail.com>
2021-12-28 15:25:43 +0530

    9th

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Elements } from "@stripe/react-stripe-js";
 
 import CheckoutForm from "./CheckoutForm";
 import "./App.css";
-import paymentIntent from "./key";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(
+  "pk_test_51KBXjTSJiJf23hBTiqnwBG461YdFhGTbqxvqXzYlO2WQFK1o6H5ZH90d6SejXEbuFPPCzQYZQypSd1hv6A2jJArw00xOo2W8M2"
+);
 
 function App() {
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
-    const key = paymentIntent.client_secret;
-    setClientSecret(key);
+    setClientSecret(
+      "sk_test_51KBXjTSJiJf23hBTl6qrq2MoMhsOGRkce9iblKCMmv3v1uzE09Jmc7uZWTGUPcbJlfonHGLEfaXK0NmPNCIHwucA00gQNCkCNx"
+    );
   }, []);
 
   const options = {
@@ -25,6 +27,7 @@ function App() {
 
   return (
     <div className="App">
+      <p>app</p>
       {clientSecret && (
         <Elements options={options} stripe={stripePromise}>
           <CheckoutForm />
